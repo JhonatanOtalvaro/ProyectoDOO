468e2eb6e46c438550c21d2e80b5aafe476b1a38
JhonatanOtalvaro <jandres.otalvarop@gmail.com>
2022-04-20 03:10:22 -0500

    Display Login

--- workiin_frontend/src/components/Login.jsx
+++ workiin_frontend/src/components/Login.jsx
@@ -8,8 +8,32 @@ import logo from '../assets/react-logo.png';
 
 const Login = () => {
   return (
-    <div>
-        Inicia sesión
+    <div className="flex justify-start items-center flex-col h-screen">
+        <div className="relative w-full h-full">
+            <img src={ucc} alt="bg" 
+              type="img"
+              className="w-full h-full object-cover"  
+            />
+            <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+                <div className="p-5">
+                    <img src={logo} alt="logo" width="130px"/>
+                </div>
+                <div className="shadow-2xl">
+                    <GoogleLogin 
+                        clientId=''
+                        render={(renderProps)=>(
+                            <button
+                             type="button"
+                             className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+
+                            >
+                                <FcGoogle className="mr-4"/> Inicia sesión con Google
+                            </button>
+                        )}
+                    />
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
