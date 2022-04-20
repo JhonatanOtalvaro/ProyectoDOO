4a81f7db6e5ccebcf59fd64dadf5a51ac4dff08b
JhonatanOtalvaro <jandres.otalvarop@gmail.com>
2022-04-20 02:50:51 -0500

    Paquetes Login

--- workiin_frontend/src/components/Login.jsx
+++ workiin_frontend/src/components/Login.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import GoogleLogin from 'react-google-login';
+import { useNavigate } from 'react-router-dom';
+import { FcGoogle } from 'react-icons/fc';
 
 const Login = () => {
   return (
-    <div>Inicia sesión</div>
+    <div>
+        Inicia sesión
+    </div>
   )
 }
 
