cabe61365e8f37e1acc2845496c5a2591b4e85ef
JhonatanOtalvaro <jandres.otalvarop@gmail.com>
2022-04-20 04:14:39 -0500

    Pins para la vista de todos los usuarios

--- workiin_frontend/src/container/Home.jsx
+++ workiin_frontend/src/container/Home.jsx
@@ -4,6 +4,8 @@ import { AiFillCloseCircle } from 'react-icons/ai';
 import { Link, Route, Routes } from 'react-router-dom';
 
 import {Sidebar, UserProfile} from '../components';
+import { client } from '../client';
+import logo from '../assets/logo.png';
 
 
 
