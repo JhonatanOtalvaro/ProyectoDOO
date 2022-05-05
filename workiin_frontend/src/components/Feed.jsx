c149b72ed9140f43492f7daecf3ea4f7b18f5c6e
JhonatanOtalvaro <jandres.otalvarop@gmail.com>
2022-05-05 13:33:59 -0500

    Loader

--- workiin_frontend/src/components/Feed.jsx
+++ workiin_frontend/src/components/Feed.jsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { useParams } from 'react-router-dom';
+
+import { client } from '../client';
+import MasonryLayout from './MasonryLayout';
+import { InfinitySpin } from 'react-loader-spinner';
+
 
 const Feed = () => {
+  const [loading, setloading] = useState(true);
+
+  if(loading) return <InfinitySpin message="Cargando los Perfiles."/>
+
   return (
     <div>Feed</div>
   )
